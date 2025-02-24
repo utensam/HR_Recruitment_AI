6754f95e0fbaf30a4dfcc84fdc664b93d5af2e4b
Uten Angyu <utenmy@gmail.com>
2025-02-24 02:26:07 +0100

    Fix: Set port for Render deployment

--- main.py
+++ main.py
@@ -116,4 +116,7 @@ def upload_file():
 
 
 if __name__ == '__main__':
+    if __name__ == '__main__':
+        port = int(os.environ.get("PORT", 10000))  # Default to 10000 if PORT is not set
+        app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
